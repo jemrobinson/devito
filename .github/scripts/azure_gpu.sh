5cf835832e848fd9a99e3dfeaa595275059c44ba
James Robinson <james.em.robinson@gmail.com>
2020-01-10 10:44:48 +0000

    Test final line of stdout

--- .github/scripts/azure_gpu.sh
+++ .github/scripts/azure_gpu.sh
@@ -1,8 +1,21 @@
-#! /bin/sh
-echo  "In azure_gpu.sh"
-echo $1 $2
-echo  "End azure_gpu.sh"
+#! /bin/bash
 
-for i in `seq 1 100`; do
-    echo  "azure_gpu.sh $i"
-done
+# Function that will perform the relevant test
+function test_gpu {
+    echo "test_gpu arguments are: $@"
+    if [[ "$1" -eq "hello" ]]; then
+        echo "Function will return 0"
+        return 0
+    fi
+    echo "Function will return 1"
+    return 1
+}
+
+
+# Run the test and return output depending on the return value
+test_gpu "$@"
+if [ $? ]; then
+    echo "SUCCESS"
+else
+    echo "FAILURE"
+fi
